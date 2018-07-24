33711646e0229c7c4ee4c761b76da98534d0b94f
Chris Mears <chris.mears@gmail.com>
2018-07-24 16:58:07 -0700

    Ensure User emails are unique ✅

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_presence_of :email, :password_digest
+  validates :email, uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_presence_of(:password_digest) }
 end
