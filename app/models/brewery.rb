d1f4e537c82cb83b2bf2c2bbcee2bfdb16f80a67
Chris Mears <chris.mears@gmail.com>
2018-08-11 14:31:06 -0700

    Add city and state to word_start searching and update scopes

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,12 +2,12 @@
 
 class Brewery < ApplicationRecord
   # Elastic Search via Searchkick
-  searchkick word_start: [:name]
+  searchkick word_start: %i[name city state]
 
   validates :name, presence: true
 
-  scope :by_city, ->(city) { where('lower(city) = ?', city.downcase) }
+  scope :by_city, ->(city) { where('lower(city) LIKE ?', "%#{city.downcase}%") }
   scope :by_name, ->(name) { where('lower(name) LIKE ?', "%#{name.downcase}%") }
-  scope :by_state, ->(state) { where('lower(state) = ?', state.downcase) }
+  scope :by_state, ->(state) { where('lower(state) LIKE ?', "%#{state.downcase}%") }
   scope :by_type, ->(type) { where('lower(brewery_type) = ?', type.downcase) }
 end
