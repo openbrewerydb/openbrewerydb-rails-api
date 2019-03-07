ffdcf5fe96b0edf5aed4b34a166710131acaef9d
Chris Mears <chris.mears@gmail.com>
2019-03-07 09:27:03 -0500

    Fix `by_state` Brewery filter to be more exact

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -16,7 +16,7 @@ class Brewery < ApplicationRecord
 
   scope :by_city, ->(city) { where('lower(city) LIKE ?', "%#{city.downcase}%") }
   scope :by_name, ->(name) { where('lower(name) LIKE ?', "%#{name.downcase}%") }
-  scope :by_state, ->(state) { where('lower(state) LIKE ?', "%#{state.downcase}%") }
+  scope :by_state, ->(state) { where('lower(state) LIKE ?', state.downcase) }
   scope :by_type, ->(type) { where('lower(brewery_type) = ?', type.downcase) }
   scope :by_tag, ->(tag) { tagged_with(tag.downcase) }
   scope :by_tags, ->(tags) { tagged_with(tags.downcase.split(',')) }
