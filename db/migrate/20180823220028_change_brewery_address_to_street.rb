ab55dab5e363380c5db8daaded75e19a332be776
Chris Mears <chris.mears@gmail.com>
2021-04-06 21:00:10 -0700

    refactor: :fire: Remove DB Migrations

--- db/migrate/20180823220028_change_brewery_address_to_street.rb
+++
@@ -1,5 +0,0 @@
-class ChangeBreweryAddressToStreet < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :breweries, :address, :street
-  end
-end
--- db/migrate/20201018223536_add_columns_to_breweries.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToBreweries < ActiveRecord::Migration[5.2]
-  def change
-    add_column :breweries, :address_2, :string
-    add_column :breweries, :address_3, :string
-    add_column :breweries, :county_province, :string
-  end
-end
