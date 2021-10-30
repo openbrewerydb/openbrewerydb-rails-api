ea36a4704e20c6c7195f89f4fa0ff9e8ba046570
Jonathan Kroll <johnnyk737@gmail.com>
2021-10-30 03:07:22 +0100

    OBDB-63 - Add/Fix validations for query params (#82)
    * Adding validation for params
    
    * Adding more validations
    
    * Forcing return during validations
    
    Co-authored-by: John  Kroll <johnnyk737@Johns-MacBook-Pro.local>

---
+++ config/initializers/constants.rb
@@ -0,0 +1,15 @@
+
+BREWERY_TYPES = [
+  "micro",
+  "nano",
+  "regional",
+  "brewpub",
+  "large",
+  "planning",
+  "bar",
+  "contract",
+  "proprieter",
+  "closed"
+].freeze
+
+DISALLOWED_CHARACTERS = %w[\\ %].freeze
