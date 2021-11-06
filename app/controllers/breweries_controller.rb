b4488318f2f59c8080a34e09887adc0ea46fb14e
Chris Mears <chris.mears@gmail.com>
2021-11-05 19:14:32 -0700

    :wastebasket: Cleanup (#89)
    * chore: :wastebasket: Remove unnecessary CRUD
    
    * docs: Update bug report template
    
    * chore: Rubocop updates

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -48,7 +48,7 @@ class BreweriesController < ApplicationController
   # GET /breweries/random
   def random
     expires_in 1.day, public: true
-    size = params[:size].to_i == 0 ? 1 : params[:size].to_i
+    size = params[:size].to_i.zero? ? 1 : params[:size].to_i
 
     # ActiveRecord random record: https://stackoverflow.com/a/25577054
     @breweries = Brewery.order(Arel.sql('RANDOM()')).limit([size, MAX_PER_PAGE].min)
@@ -88,27 +88,6 @@ class BreweriesController < ApplicationController
       end
 
     json_response(@breweries)
-  end
-
-  # POST /breweries
-  # NOTE: Disabled via /config/routes.rb
-  def create
-    @brewery = Brewery.create!(brewery_params)
-    json_response(@brewery, :created)
-  end
-
-  # PUT /breweries/:id
-  # NOTE: Disabled via /config/routes.rb
-  def update
-    @brewery.update(brewery_params)
-    head :no_content
-  end
-
-  # DELETE /breweries/:id
-  # NOTE: Disabled  via /config/routes.rb
-  def destroy
-    @brewery.destroy
-    head :no_content
   end
 
   private
