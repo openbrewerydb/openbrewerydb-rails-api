360a2d25627563e440be4e48978e55d1e9df478a
Chris J Mears <chris.mears@gmail.com>
2023-01-07 09:42:46 -0800

    Remove pg_search
    This never worked as-intended and I also plan to build an ES indexing workflow.

--- db/schema.rb
+++ db/schema.rb
@@ -29,13 +29,4 @@ ActiveRecord::Schema.define(version: 2023_01_07_000000) do
     t.text "tags"
     t.index ["obdb_id"], name: "breweries_obdb_id_key", unique: true
   end
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
-  end
 end
