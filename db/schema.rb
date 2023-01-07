483b5864f2f40014c39bc5e59410d8c2c7feee4b
Chris J Mears <chris.mears@gmail.com>
2023-01-07 09:41:42 -0800

    Remove users

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,6 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
+# rake db:schema:load
 
-ActiveRecord::Schema.define(version: 2021_04_07_030557) do
+ActiveRecord::Schema.define(version: 2023_01_07_000000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -47,12 +37,5 @@ ActiveRecord::Schema.define(version: 2021_04_07_030557) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 end
