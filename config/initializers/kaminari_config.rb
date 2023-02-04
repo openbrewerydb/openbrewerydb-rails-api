4efc07010bdd410bb2a3bb013ed214c608845bef
Chris J Mears <chris.mears@gmail.com>
2023-02-03 18:01:17 -0800

    Remove unnecessary commented out configs

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -3,10 +3,4 @@
 Kaminari.configure do |config|
   config.default_per_page = 50
   config.max_per_page = 200
-  # config.outer_window = 0
-  # config.left = 0
-  # config.right = 0
-  # config.page_method_name = :page
-  # config.param_name = :page
-  # config.params_on_first_page = false
 end
