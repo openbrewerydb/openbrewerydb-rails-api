c22f46b75b5308022469e0d6130c5f7a7746b45e
Chris Mears <chris.mears@gmail.com>
2023-03-16 19:58:21 -0700

    Merge Staging (#122)
    * feat: Combine param validation, return JSON
    
    Allow for multiple error messages and use validation for search endpoints too
    
    * fix: Fix typo
    
    * feat: :fire: Remove unnecessary query param check
    
    * Should be proprietOr instead of proprieter (#121)
    
    * chore: Rubocop fixes
    
    ---------
    
    Co-authored-by: Anthony Laflamme <anthonylaflammex@gmail.com>

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
 
-BREWERY_TYPES = [
-  "micro",
-  "nano",
-  "regional",
-  "brewpub",
-  "large",
-  "planning",
-  "bar",
-  "contract",
-  "proprieter",
-  "closed"
+BREWERY_TYPES = %w[
+  micro
+  nano
+  regional
+  brewpub
+  large
+  planning
+  bar
+  contract
+  proprietor
+  closed
 ].freeze
 
 MAX_PER_PAGE = 50
